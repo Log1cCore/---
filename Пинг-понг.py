00c03aa7b8823a4a4676c66f022ab1e1b2fc95b7
Никита <Nikita.958@yandex.ru>
2025-04-11 20:04:35 +0300

    Update Пинг-понг.py
    Добавлен класс Enemy, который отвечает за перемещение мяча. Добавлена обработка победы и ее текст при укатывании мяча за пределы экрана

--- Пинг-понг.py
+++ Пинг-понг.py
@@ -18,18 +18,49 @@ class Player(GameSprite):
         super().__init__(player_image, player_x, player_y, player_speed, width, height)
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 850:
+        if keys[K_s] and self.rect.y < 695:
             self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 850:
+        if keys[K_DOWN] and self.rect.y < 695:
             self.rect.y += self.speed        
 
+class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__(player_image, player_x, player_y, player_speed, width, height)
+        self.direction = 'right'
+        self.direction1 = 'up'
+    def update(self):
+        if sprite.collide_rect(ball, racket1):
+            self.direction = 'right'
+        if sprite.collide_rect(ball, racket2):
+            self.direction = 'left'
+        if self.rect.y >= 850:
+            self.direction1 = 'up'
+        if self.rect.y <= 0:
+            self.direction1 = 'down'
+        if self.direction == 'right':
+            self.rect.x += self.speed
+        else:
+            self.rect.x -= self.speed
+        if self.direction1 == 'up':
+            self.rect.y -= self.speed
+        else:
+            self.rect.y += self.speed
+
+racket1 = Player('racket.png', 50, 350, 4, 40, 200)
+racket2 = Player('racket.png', 840, 350, 4, 40, 200)
+ball = Enemy('ball.png', 450, 450, 3, 50, 50)
+
+font.init()
+win = font.Font(None, 70)
+
 game = True
+finish = False
 clock = time.Clock()
 FPS = 60
 while game:
@@ -37,5 +68,25 @@ while game:
         if e.type == QUIT:
             game = False
     window.fill((179, 166, 226))
+
+    if ball.rect.x >= 850:
+        win_text = win.render('Левый игрок выиграл!', True, (255, 41, 79))
+        window.blit(win_text, (250, 400))
+        finish = True
+    if ball.rect.x <= 0:
+        win_text = win.render('Правый игрок выиграл!', True, (255, 41, 79))
+        window.blit(win_text, (250, 400))
+        finish = True
+
+    
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+    if not finish:
+        racket1.update_l()
+        racket2.update_r()
+        ball.update()
+
     display.update()
     clock.tick(FPS)
